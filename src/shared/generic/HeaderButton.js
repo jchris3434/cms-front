a75b5d8571b5ce1dddb391febd659954fc874469
Hemy Day <sire.amanda@gmail.com>
2024-03-05 11:09:54 +0100

    New version using React-Bootstrap

--- src/shared/generic/HeaderButton.js
+++ src/shared/generic/HeaderButton.js
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
 
 /**
  * @description                 The buttons that are displayed in the header navbar
  * 
  * @param {object}  props 
- * @param {boolean} darkmode    props.darkmode  Either 'true' or 'false'
  * @param {string}  icon        props.icon      The reference of the icon from the Boostrap library (example : bi-screwdriver)
  * @param {string}  text        props.text      The text that is displayed on the button
  * @param {string}  redirect    props.redirect  The link that the button redirects to
@@ -12,63 +12,18 @@ import React, { useState } from 'react';
  */
 
 function HeaderButton(props){
-
-    const [isHover, setIsHover] = useState(false);
-
-    const handleMouseEnter = () => {
-        setIsHover(true);
-    };
-    const handleMouseLeave = () => {
-        setIsHover(false);
+    
+    const redirect = () => {
+        window.location.href = props.redirect;
     };
 
-    return (
-        <button 
-        onMouseEnter={handleMouseEnter}
-        onMouseLeave={handleMouseLeave}
-        style={{
-            display:'inline-block',
-            width:'100px', 
-            height:'90px', 
-            borderRadius:'5px',
-            backgroundColor: props.darkmode== true ? (isHover == true ? 'var(--DTPink)' : 'var(--DTDarkGray)') : (isHover == true ? 'var(--LTPink)' : 'var(--LTWhiteLighter)'),
-            padding:'10px 10px',
-            alignContent:'center',
-            verticalAlign: 'middle',
-            border:'none',
-            cursor:'pointer'
-        }}> 
-       
-            
-            <div
-            style={{
-                textAlign: 'center',
-                marginBottom:'5px'
-            }}>
-            
-                <i className={props.icon}
-                style={{
-                    color: props.darkmode== true ? ('var(--DTWhiteDarker)') : (isHover == true ? 'var(--LTWhiteLighter)' : 'var(--LTPink)'),
-                    fontSize:'40px',
-                    lineHeight:'40px',
-                }}
-                ></i>
-            </div>
-            
-            <p
-            style={{
-                color : props.darkmode== true ? ('var(--DTWhiteDarker)') : (isHover == true ? 'var(--LTWhiteLighter)' : 'var(--LTPink)'),
-                textAlign:'center',
-                fontSize:'16px',
-                lineHeight:'16px',
-                margin:'0'   
-            }}>
-                    
-                {props.text}
-                
-            </p>
-        </button>
+    return ( 
+        <Button className='HeaderButton' onClick={redirect}> 
+            <i className={props.icon}></i>
+            <label>{props.text}</label>
+        </Button>
     )
 }
 
 export default HeaderButton ;
+
