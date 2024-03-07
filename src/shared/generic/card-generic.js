9557e264c954e3e57ec4075cc35f78e67ea61d66
Sofian <bwb.s.aghmiri@fondespierre.com>
2024-03-07 14:16:56 +0100

    Initial commit, add new file

---
+++ src/shared/generic/card-generic.js
@@ -0,0 +1,17 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import { Card } from 'react-bootstrap';
+
+function CardGeneric(props) {
+    return (
+        <div className={`CardGeneric ${props.darkMode ? 'darkmode' : ''}`}>
+            <Card style={{ width: '18rem' }}>
+                <Card.Body>
+                {props.children}
+                </Card.Body>
+            </Card>
+        </div>
+    );
+}
+
+export default CardGeneric;
