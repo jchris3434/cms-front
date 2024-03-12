09dd318d1ef96f04bb67e509f3b7d2659eec650f
Sofian <bwb.s.aghmiri@fondespierre.com>
2024-03-12 15:35:49 +0100

    DashboardCard Js and css was added

--- src/shared/component/DashBoardCard.js
+++ src/shared/component/DashBoardCard.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import CardGeneric from '../generic/card-generic'; // Import the CardGeneric component
+import CardGeneric from '../generic/CardGeneric'; // Import the CardGeneric component
 import { Card } from 'react-bootstrap'; // Import the Card component from React Bootstrap
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BsFillPersonFill } from 'react-icons/bs'; // Import the icon from react-icons package
-import './dashboard-card.css';
-
+import './DashboardCard.css';
 
 function DashBoardCard() {
     return (
-        <CardGeneric darkMode={true} className="card-generic"> 
+        <CardGeneric darkMode={false} className="card-generic"> 
             {/* Ajoutez la classe "card-generic" pour appliquer l'ombre */}
             <Card.Body className="card-body-container"> 
                 <div className="icon icon-large mr-2 mt-1"> 
