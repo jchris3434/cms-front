522c3e2d1bbefbc7fd85556e6a40587e9cfbaf6f
Sofian <bwb.s.aghmiri@fondespierre.com>
2024-03-12 11:17:04 +0100

    Test cardgeneric  in index.js was deleted

--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import "bootstrap-icons/font/bootstrap-icons.css";
-import CardGeneric from './shared/generic/CardGeneric';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
