bff0516e0d8d4a8fe56bc540e236aaa8efebd4ec
Sofian <bwb.s.aghmiri@fondespierre.com>
2024-03-12 10:34:15 +0100

    modification darkmode

--- src/shared/generic/CardGeneric.js
+++ src/shared/generic/CardGeneric.js
@@ -1,15 +1,18 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import { Card } from 'react-bootstrap';
-import './CardGeneric.css';
-
+import './CardGeneric.css'
+import '../../index.css';
 
 function CardGeneric(props) {
+    // Déterminez les classes à appliquer en fonction de l'état du mode sombre
+    const cardClasses = `CardGeneric ${props.darkMode ? 'darkmode' : ''}`;
+
     return (
-        <div className={`CardGeneric ${props.darkMode ? 'darkmode' : ''}`}>
-            <Card>
-                <Card.Body> 
-                {props.children}
+        <div className="CardContainer">
+            <Card className={cardClasses}>
+                <Card.Body>
+                    {props.children}
                 </Card.Body>
             </Card>
         </div>
@@ -17,3 +20,4 @@ function CardGeneric(props) {
 }
 
 export default CardGeneric;
+
