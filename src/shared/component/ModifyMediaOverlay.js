4ea57147b2a9b6caa54ee705a43b295cfe8b0ebd
JossBeweb <bwb.j.kerambellec@fondespierre.com>
2024-03-14 16:17:02 +0100

    Added disappear function on button Retour

--- src/shared/component/ModifyMediaOverlay.js
+++ src/shared/component/ModifyMediaOverlay.js
@@ -2,8 +2,7 @@ import React from 'react';
 import './ModifyMediaOverlay.css';
 
 function ModifyMediaOverlay(props){
-    // console.log('ModifMediaIverlay DM : '+props.isDarkMode)
-    
+ 
     return ( 
        <div class="modifymediaoverlay" id={props.isDarkMode === true ? 'dark':''}>
         <h3>Modifier un media</h3>
@@ -19,12 +18,11 @@ function ModifyMediaOverlay(props){
          </div>
       </div>
       <div id="modifymediabuttons">
-      <button>Retour</button>
+      <button onClick={() => props.setShowModifyMedia(false)}>Retour</button> 
       <button type="submit">Valider</button>
       </div>
         </form>
        </div>
-    
     )
 }
 
