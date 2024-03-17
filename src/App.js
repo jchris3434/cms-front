6c2f14353e5a79cad40144ed6a30f8310b63458a
jchris3434 <jc.fontaine34@gmail.com>
2024-03-17 13:23:09 +0100

    CMS-134-SidebarLeftNavigation

--- src/App.js
+++ src/App.js
@@ -3,13 +3,23 @@ import React, { useState } from 'react';
 import getDarkModeFromLocalStorage from './shared/getDarkModeFromLocalStorage'
 
 import Header from './shared/component/Header'
+import SidebarLeftNavigation from './shared/component/SidebarLeftNavigation';
 
 function App() {
   const [isdarkmode, setDarkMode] = useState(getDarkModeFromLocalStorage())
+  const pages = [
+    { url: '/dashboard-projet', label: 'Dashboard projet' },
+    { url: '/creation-page', label: 'Création de page' },
+    { url: '/liste-medias', label: 'Liste des médias' },
+    { url: '/liste-pages', label: 'Liste des pages' },
+    { url: '/preview', label: 'Preview' }
+];
+
   return (
-    <div>
+    <section className='section'>
       <Header isDarkMode={isdarkmode} setDarkMode={setDarkMode}/>
-    </div>
+      <SidebarLeftNavigation isDarkMode={isdarkmode} items={pages}/>
+    </section>
   );
 }
 
---
+++ src/shared/component/SidebarLeftNavigation.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import './SidebarLeftNavigation.css';
+import PropTypes from 'prop-types';
+import { BsArrowLeft, BsArrowRight } from 'react-icons/bs';
+
+const SidebarLeftNavigation = ({ items, isDarkMode }) => {
+    const [isOpen, setIsOpen] = useState(true);
+    const [contentDisplay, setContentDisplay] = useState('');
+
+    const toggleSidebar = () => {
+        setIsOpen(!isOpen);
+        setContentDisplay(isOpen ? 'classClose' : '');
+    };
+
+    return (
+        <div className={`sidebar-left ${contentDisplay} ${isDarkMode ? 'dark-mode' : 'light-mode'}`}>
+            <div className={`content ${contentDisplay}`}>
+                <h5 className="pages-list-title">Navigation</h5>
+                <ul className="pages-list">
+                    {items.map((item, index) => (
+                        <div className={`pages-bubble ${isDarkMode ? 'dark-mode' : 'light-mode'}`} key={index}>
+                            <span href={item.url}>{item.label}</span>
+                        </div>
+                    ))}
+                </ul>
+            </div>
+            <div className={`button-container ${contentDisplay}`}>
+            <button className={`toggle-btn ${isDarkMode ? 'dark-mode' : ''}`} onClick={toggleSidebar}>
+                    {isOpen ? <BsArrowLeft /> : <BsArrowRight />}
+                </button>
+            </div>
+        </div>
+    );
+};
+
+SidebarLeftNavigation.propTypes = {
+    items: PropTypes.arrayOf(
+        PropTypes.shape({
+            url: PropTypes.string.isRequired,
+            label: PropTypes.string.isRequired
+        })
+    ).isRequired,
+    isDarkMode: PropTypes.bool.isRequired
+};
+
+export default SidebarLeftNavigation;
