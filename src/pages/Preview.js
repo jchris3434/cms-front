83f5a6250bd19622634d974508131b371c9c2c78
Enzo <bwb.e.panarotto@fondespierre.com>
2024-03-19 17:02:37 +0100

    Initial commit

--- src/pages/Preview.js
+++ src/pages/Preview.js
@@ -1,7 +1,20 @@
+import SidebarLeftNavigation from '../shared/component/SidebarLeftNavigation' 
+
 function Preview(props) {
+  const pages = [
+    { url: '/', label: 'Dashboard projet' },
+    { url: '/createPage', label: 'Création de page' },
+    { url: '/mediasList', label: 'Liste des médias' },
+    { url: '/pagesList', label: 'Liste des pages' },
+    { url: '/preview', label: 'Preview' }
+];
     return (
       <div>
-        <h1>This is the preview page</h1>
+        <div>
+          <SidebarLeftNavigation isDarkMode={props.isDarkMode} items={pages}/>
+        </div>
+      <div>
+        <h1>This is the preview page</h1></div>
       </div>
     );
   }
