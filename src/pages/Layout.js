51b3d43be8e1bce1339f3aed84520ca8a4fe22a3
Sofian <bwb.s.aghmiri@fondespierre.com>
2024-03-21 13:59:15 +0100

    add hearderlogin

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import Header from '../shared/component/Header';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
+import HeaderLogin from '../shared/component/HeaderLogin';
 
-function Layout(props) {
-    return (
-      <div>
-        <Header isDarkMode={props.isDarkMode} setDarkMode={props.setDarkMode}/>
-        <Outlet isDarkMode={props.isDarkMode} setDarkMode={props.setDarkMode}/>
-      </div>
-    );
-  }
-   export default Layout;
+function Layout(props) {  
+  const location = useLocation();  
+  const isLoginPage = location.pathname === '/'; 
+  
+  return (    
+    <div>      
+      {isLoginPage ? (
+        <>  
+          <HeaderLogin isDarkMode={props.isDarkMode} setDarkMode={props.setDarkMode} />
+          <Outlet isDarkMode={props.isDarkMode} setDarkMode={props.setDarkMode} />     
+        </>
+      ) : (        
+        <div>         
+          <Header isDarkMode={props.isDarkMode} setDarkMode={props.setDarkMode} />          
+          <Outlet isDarkMode={props.isDarkMode} setDarkMode={props.setDarkMode} />        
+        </div>
+      )}    
+    </div>  
+  );
+}
+
+export default Layout;
