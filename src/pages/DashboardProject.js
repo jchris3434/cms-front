08b332589e446a3ec8d73b98a63cedca3a19f627
Enzo <bwb.e.panarotto@fondespierre.com>
2024-04-05 11:21:26 +0200

    DashboardProject changes

--- src/pages/DashboardProject.js
+++ src/pages/DashboardProject.js
@@ -1,82 +1,82 @@
 import React, { useState } from 'react';
-import ContainerCustom from '../shared/generic/ContainerCustom'
-import DashBoardCard from '../shared/component/DashBoardCard'
-import Overlay from '../shared/component/Overlay'
+import ContainerCustom from '../shared/generic/ContainerCustom';
+import DashBoardCard from '../shared/component/DashBoardCard';
+import Overlay from '../shared/component/Overlay';
 import PropTypes from 'prop-types';
-import DeleteProjectOverlay from '../shared/component/DeleteProjectOverlay'
-import CreatePageOverlay from '../shared/component/CreatePageOverlay'
+import DeleteProjectOverlay from '../shared/component/DeleteProjectOverlay';
+import CreatePageOverlay from '../shared/component/CreatePageOverlay';
 import 'bootstrap/dist/css/bootstrap.css';
-import './DashboardProject.css'
+import './DashboardProject.css';
 
-export default function DashboardProject(props) {
-  const [showdeleteprojectoverlay, setShowDeleteProjectOverlay] = useState(false);
-  const [showcreatepageoverlay, setShowCreatePageOverlay] = useState(false);
+export default function DashboardProject({ isDarkMode }) {
+  // Destructuring the useState calls symmetrically
+  const [showDeleteProjectOverlay, setShowDeleteProjectOverlay] = useState(false);
+  const [showCreatePageOverlay, setShowCreatePageOverlay] = useState(false);
 
-    return (
-      <div id={props.isDarkMode === true ? 'dashboardprojectdark':''}>
-        <div className="rounded-pill" id="projectstatusbar">
+  return (
+    <div id={isDarkMode ? 'dashboardprojectdark' : ''}>
+      <div className="rounded-pill" id="projectstatusbar">
         <span>Projet: Blog animalier</span>
         <span>Client: Sue Flay</span>
         <span>Statut: Développement</span>
-        </div>
-
-        <ContainerCustom displayStyle='dashboard' isDarkMode={props.isDarkMode}>
-            <DashBoardCard 
-              isDarkMode={props.isDarkMode} 
-              text='Accéder à la liste de toutes les pages de ce projet' 
-              title='Liste Des pages'
-              icon="bi bi-folder-fill"
-              redirect='/pagesList'
-            />
-      
-            <DashBoardCard 
-              isDarkMode={props.isDarkMode}
-              text='Accéder au mode création de pages'
-              title='Créer une page'
-              icon="bi bi-plus-circle-fill"
-              show={setShowCreatePageOverlay} 
-            />
-            <DashBoardCard 
-              isDarkMode={props.isDarkMode}
-              title='Preview'
-              text='Accéder au mode preview de votre site'
-              icon="bi bi-eye-fill"
-              redirect='/preview'
-            />
-            <DashBoardCard 
-              isDarkMode={props.isDarkMode}
-              title='Supprimer'
-              text='Supprimer le projet en cours'
-              icon="bi-x-circle-fill"
-              show={setShowDeleteProjectOverlay} 
-            />
-        </ContainerCustom> 
-
-        {/* Overlays */}
-         {showdeleteprojectoverlay && 
-           <Overlay 
-           component={
-              <DeleteProjectOverlay 
-                  isDarkMode={props.isDarkMode}
-                  setShowDeleteProjectOverlay={setShowDeleteProjectOverlay}
-              />}
-           />
-            }
-
-          {showcreatepageoverlay && 
-           <Overlay 
-           component={
-              <CreatePageOverlay 
-                  isDarkMode={props.isDarkMode}
-                  setShowCreatePageOverlay={setShowCreatePageOverlay}
-              />}
-           />
-            }
-
-
       </div>
-    );
-  }
-  DashboardProject.propTypes = {
-    isDarkMode: PropTypes.bool.isRequired // isDarkMode prop is required and should be a boolean
-  };
+
+      <ContainerCustom displayStyle='dashboard' isDarkMode={isDarkMode}>
+        <DashBoardCard
+          isDarkMode={isDarkMode}
+          text='Accéder à la liste de toutes les pages de ce projet'
+          title='Liste Des pages'
+          icon="bi bi-folder-fill"
+          redirect='/pagesList'
+        />
+
+        <DashBoardCard
+          isDarkMode={isDarkMode}
+          text='Accéder au mode création de pages'
+          title='Créer une page'
+          icon="bi bi-plus-circle-fill"
+          show={setShowCreatePageOverlay}
+        />
+        <DashBoardCard
+          isDarkMode={isDarkMode}
+          title='Preview'
+          text='Accéder au mode preview de votre site'
+          icon="bi bi-eye-fill"
+          redirect='/preview'
+        />
+        <DashBoardCard
+          isDarkMode={isDarkMode}
+          title='Supprimer'
+          text='Supprimer le projet en cours'
+          icon="bi-x-circle-fill"
+          show={setShowDeleteProjectOverlay}
+        />
+      </ContainerCustom>
+
+      {/* Overlays */}
+      {showDeleteProjectOverlay &&
+        <Overlay
+          component={
+            <DeleteProjectOverlay
+              isDarkMode={isDarkMode}
+              setShowDeleteProjectOverlay={setShowDeleteProjectOverlay}
+            />}
+        />
+      }
+
+      {showCreatePageOverlay &&
+        <Overlay
+          component={
+            <CreatePageOverlay
+              isDarkMode={isDarkMode}
+              setShowCreatePageOverlay={setShowCreatePageOverlay}
+            />}
+        />
+      }
+    </div>
+  );
+}
+
+DashboardProject.propTypes = {
+  isDarkMode: PropTypes.bool.isRequired // isDarkMode prop is required and should be a boolean
+};
