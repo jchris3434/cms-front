ecaf3dea637bb92d92838a85db965ecb2953fb45
Enzo <bwb.e.panarotto@fondespierre.com>
2024-04-05 11:32:42 +0200

    PagesList changes

--- src/pages/PagesList.js
+++ src/pages/PagesList.js
@@ -1,80 +1,74 @@
-import React, { useState, useEffect } from 'react'
-import ContainerCustom from '../shared/generic/ContainerCustom'
-import PageCard from '../shared/component/PageCard'
+import React, { useState, useEffect } from 'react';
+import ContainerCustom from '../shared/generic/ContainerCustom';
+import PageCard from '../shared/component/PageCard';
 import SidebarLeftNavigation from '../shared/component/SidebarLeftNavigation';
-import SearchBar from'../shared/generic/SearchBar'
+import SearchBar from '../shared/generic/SearchBar';
 import './PagesList.css';
-import Overlay from '../shared/component/Overlay'
-import CreatePageOverlay from '../shared/component/CreatePageOverlay'
-import AddButton from '../shared/component/AddButton'
+import Overlay from '../shared/component/Overlay';
+import CreatePageOverlay from '../shared/component/CreatePageOverlay';
+import AddButton from '../shared/component/AddButton';
 import PropTypes from 'prop-types';
 
-function PageList(props) {
-  
-  const pages = [
-    { url: '/dashboard', label: 'Dashboard projet' },
-    { url: '/createPage', label: 'Création de page' },
-    // { url: '/mediasList', label: 'Liste des médias' },
-    { url: '/pagesList', label: 'Liste des pages' },
-    { url: '/preview', label: 'Preview' }
-];
+function PagesList({ isDarkMode }) {
+  const [showCreatePageOverlay, setShowCreatePageOverlay] = useState(false);
+  const [pagesList, setPagesList] = useState([]);
 
-const [showcreatepageoverlay, setShowCreatePageOverlay] = useState(false);
+  useEffect(() => {
+    fetch("http://localhost:12000/pages")
+      .then((res) => res.json())
+      .then((res) => setPagesList(res))
+      .catch((err) => console.error(err));
+  }, []);
 
-const [pagesList, setPagesList] = useState([]);
-   useEffect(()=> {
-      fetch("http://localhost:12000/pages")
-              .then((res) => res.json())
-              .then((res) => {setPagesList(res)})
-              .catch((err) => console.error(err))
-      }, [])
-
-    return (
-      <div className='pagelist' id={props.isDarkMode === true ? 'pageslistdark':''}>
-        <SidebarLeftNavigation isDarkMode={props.isDarkMode} items={pages}/>
-        <div id='contentpagelistitems'>
-          <div>
-              <SearchBar customWidth='22.5rem' isDarkMode={props.isDarkMode} placeholder='Rechercher une page'/>
-              <AddButton 
-                show={setShowCreatePageOverlay} 
-                text='Ajouter '
-                id='addbuttonpagelist'
-              />
-          </div>
-          <div>
-          <ContainerCustom 
-              isDarkMode={props.isDarkMode} 
+  return (
+    <div className='pagelist' id={isDarkMode ? 'pageslistdark' : ''}>
+      <SidebarLeftNavigation isDarkMode={isDarkMode} items={[
+        { url: '/dashboard', label: 'Dashboard projet' },
+        { url: '/createPage', label: 'Création de page' },
+        // { url: '/mediasList', label: 'Liste des médias' },
+        { url: '/pagesList', label: 'Liste des pages' },
+        { url: '/preview', label: 'Preview' }
+      ]} />
+      <div id='contentpagelistitems'>
+        <div>
+          <SearchBar customWidth='22.5rem' isDarkMode={isDarkMode} placeholder='Rechercher une page' />
+          <AddButton
+            show={setShowCreatePageOverlay}
+            text='Ajouter '
+            id='addbuttonpagelist'
+          />
+        </div>
+        <div>
+          <ContainerCustom
+            isDarkMode={isDarkMode}
           >
-            {pagesList.map((page) => {
-return (
-  <PageCard
-    key={page.pag_id} // En supposant que page.pag_id est un identifiant unique pour chaque page
-    id={`page${page.pag_id}`}
-    pageName={page.pag_name}
-    isDarkMode={props.isDarkMode}
-  />
-);
-            })}
+            {pagesList.map((page) => (
+              <PageCard
+                key={page.pag_id}
+                id={`page${page.pag_id}`}
+                pageName={page.pag_name}
+                isDarkMode={isDarkMode}
+              />
+            ))}
           </ContainerCustom>
         </div>
-        </div>
-        
-      {showcreatepageoverlay && 
-           <Overlay 
-           component={
-              <CreatePageOverlay 
-                  isDarkMode={props.isDarkMode} 
-                  setShowCreatePageOverlay={setShowCreatePageOverlay}
-                  pagesList={pagesList}
-              />}
-           />
-            }
       </div>
-      
-      
-    );
-  }
-  PageList.propTypes = {
+      {showCreatePageOverlay &&
+        <Overlay
+          component={
+            <CreatePageOverlay
+              isDarkMode={isDarkMode}
+              setShowCreatePageOverlay={setShowCreatePageOverlay}
+              pagesList={pagesList}
+            />}
+        />
+      }
+    </div>
+  );
+}
+
+PagesList.propTypes = {
   isDarkMode: PropTypes.bool.isRequired // isDarkMode prop is required and should be a boolean
 };
-   export default PageList;
+
+export default PagesList;
