f51107c7f5fa24d42d881ec89ed2a44c9bcc1722
Enzo <bwb.e.panarotto@fondespierre.com>
2024-04-11 11:57:50 +0200

    themeButton test2

--- src/shared/generic/ThemeButton.js
+++ src/shared/generic/ThemeButton.js
@@ -7,33 +7,36 @@ import PropTypes from 'prop-types';
  * @description                     Theme Button for changing To Light/Dark Mode 
  * 
  * @param {object} props
- * @param {boolean} isdarkmode      props.isDarkMode either 'true' or 'false'
+ * @param {boolean} isDarkMode      props.isDarkMode either 'true' or 'false'
  * @param {function} setDarkMode    props.setDarkMode UseState in parent element
  * 
  * @returns                         JSX elements (ThemeButton)
  */
 
 function ThemeButton(props) {
-	function toggleMode() {
-		localStorage.setItem("isdarkmode", !getDarkModeFromLocalStorage());
+    function toggleMode() {
+        localStorage.setItem("isdarkmode", !getDarkModeFromLocalStorage());
         props.setDarkMode(getDarkModeFromLocalStorage());
-	} // Inverts boolean in localStorage, then updates state in app.jss
+    } // Inverts boolean in localStorage, then updates state in app.jss
 
-	return (
-        <label className='toggle-switch' htmlFor='darkModeToggle'>
-            <input 
-                id='darkModeToggle'
-                type='checkbox' 
-                checked={props.isdarkmode} 
-                onChange={toggleMode}
-            />
-            <span className='slider'></span>
-        </label>
-	);
+    return (
+        <div className='theme-button'>
+            <label htmlFor='darkModeToggle'>Dark Mode</label>
+            <label className='toggle-switch' htmlFor='darkModeToggle'>
+                <input 
+                    id='darkModeToggle'
+                    type='checkbox' 
+                    checked={props.isDarkMode} 
+                    onChange={toggleMode}
+                />
+                <span className='slider'></span>
+            </label>
+        </div>
+    );
 }
 
 ThemeButton.propTypes = {
-    isdarkmode: PropTypes.bool.isRequired,
+    isDarkMode: PropTypes.bool.isRequired,
     setDarkMode: PropTypes.func.isRequired
 };
 
